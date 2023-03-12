4f4d848dc489d85064fe2d70b049d874ca32f1c3
winrid <winrid@gmail.com>
2023-03-12 16:52:41 -0700

    build setup progress

--- govscentdotorg/scripts/step_1_create_dictionary.py
+++ govscentdotorg/scripts/step_1_create_dictionary.py
@@ -1,2 +1,5 @@
 # 1. We train a gensim model and create a dictionary from the latest Wikipedia dump: https://dumps.wikimedia.org/enwiki/latest/
 # 2. We create and save the dictionary (python -m gensim.scripts.make_wiki)
+
+def run(workingDir: str):
+    os.exec
