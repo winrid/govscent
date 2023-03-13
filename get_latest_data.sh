2e365ec20a82fba5acee7a34abf057f8c504fbf6
winrid <winrid@gmail.com>
2023-03-12 22:22:24 -0700

    cd to dir

--- get_latest_data.sh
+++ get_latest_data.sh
@@ -4,6 +4,7 @@
 
 #usc-run govinfo --collections=BILLS --storehtml,text --bulkdata=False
 
+cd /home/winrid/govscent
 python3.10 -m venv env
 source env/bin/activate
 python3.10 manage.py runscript usa_import_bills --script-args /home/winrid/congress/data False False
