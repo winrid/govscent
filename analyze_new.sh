f88cbce8cf0f8fa0ad86a063bd1afaeb7ff44463
winrid <winrid@gmail.com>
2023-03-26 17:48:24 -0700

    Keep only one instance of script running at a time.

--- analyze_new.sh
+++ analyze_new.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This is ran every six hours in production.
+
 previous_instance_active () {
   pgrep -a sh | grep -v "^$$ " | grep --quiet 'analyze_new'
 }
--- get_latest_data.sh
+++ get_latest_data.sh
@@ -2,12 +2,21 @@
 
 # This is ran every six hours in production.
 
-cd /home/winrid/congress
-python3 -m venv env
-source env/bin/activate
-usc-run govinfo --collections=BILLS --store=html
+previous_instance_active () {
+  pgrep -a sh | grep -v "^$$ " | grep --quiet 'get_latest_data'
+}
 
-cd /home/winrid/govscent
-python3 -m venv env
-source env/bin/activate
-python3.10 manage.py runscript usa_import_bills --script-args /home/winrid/congress/data False False
+if previous_instance_active
+then
+  date +'PID: $$ Previous instance is still active at %H:%M:%S, aborting ... '
+else
+  cd /home/winrid/congress
+  python3 -m venv env
+  source env/bin/activate
+  usc-run govinfo --collections=BILLS --store=html
+
+  cd /home/winrid/govscent
+  python3 -m venv env
+  source env/bin/activate
+  python3.10 manage.py runscript usa_import_bills --script-args /home/winrid/congress/data False False
+fi
