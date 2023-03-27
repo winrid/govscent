f0271d04d6bf071810b05833e42dc54e6b82118c
winrid <winrid@gmail.com>
2023-03-26 17:46:50 -0700

    prevent duplicate runs of cron

--- analyze_new.sh
+++ analyze_new.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cd /home/winrid/govscent
-python3 -m venv env
-source env/bin/activate
-python3.10 manage.py runscript analyze_bills --script-args False
+previous_instance_active () {
+  pgrep -a sh | grep -v "^$$ " | grep --quiet 'analyze_new'
+}
+
+if previous_instance_active
+then
+  date +'PID: $$ Previous instance is still active at %H:%M:%S, aborting ... '
+else
+  cd /home/winrid/govscent
+  python3 -m venv env
+  source env/bin/activate
+  python3.10 manage.py runscript analyze_bills --script-args False
+fi
