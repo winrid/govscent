06e921336d362621f843634d8cb2fdbfe8c70f73
winrid <winrid@gmail.com>
2023-05-17 22:19:28 -0700

    not sure how this happened

--- govscentdotorg/migrations/0017_billsection_bill_bill_sections.py
+++ govscentdotorg/migrations/0017_billsection_bill_bill_sections.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='BIllSection',
+            name='BillSection',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.TextField()),
