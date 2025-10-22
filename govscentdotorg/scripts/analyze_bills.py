38d5dd4b85f5e584399c90b00e6831b77e3b4157
winrid <winrid@gmail.com>
2025-10-22 00:16:33 -0700

    try to get shorter topics

--- govscentdotorg/scripts/analyze_bills.py
+++ govscentdotorg/scripts/analyze_bills.py
@@ -321,7 +321,7 @@ def reduce_topics(bill: Bill) -> str:
         print(f"Topic list long, reduced to {len(chunks)} chunks for {bill.gov_id} (iteration {iterations}).")
         for index, chunk in enumerate(chunks):
             print(f"Summarizing chunk {index} with {len(chunk.split(' '))} words.")
-            prompt = f"List the top 10 most important topics the following text:\n{chunk}"
+            prompt = f"List the top 10 most important topics (very short topic descriptions, less than 10 words) the following text:\n{chunk}"
             completion = openai_with_rate_limit_handling(prompt=prompt, retry=True)
             print(completion)
             response_text = completion.choices[0].message.content
@@ -345,9 +345,9 @@ def analyze_bill_sections(bill: Bill, reparse_only: bool):
             if not section.last_analyze_response:
                 print(f"Processing section {index + 1}/{len(sections)} of {bill.gov_id}")
                 # If we can, this is done all in one prompt to try to reduce # of tokens.
-                prompt = f"Summarize and list the top 10 most important topics (short topic descriptions) the following text, and rank it from 0 to 10 on staying on topic:\n{section.get_text(bill.text)}" \
+                prompt = f"Summarize and list the top 10 most important topics (short topic descriptions, less than 10 words) the following text, and rank it from 0 to 10 on staying on topic:\n{section.get_text(bill.text)}" \
                     if len(
-                    sections) == 1 else f"List the top 10 most important topics (short topic descriptions) the following text:\n{section.get_text(bill.text)}"
+                    sections) == 1 else f"List the top 10 most important topics (short topic descriptions, less than 10 words) the following text:\n{section.get_text(bill.text)}"
                 completion = openai_with_rate_limit_handling(prompt=prompt, retry=True)
                 print(completion)
                 response_text = completion.choices[0].message.content
